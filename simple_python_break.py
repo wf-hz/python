9627457909b4ac4e93af2996e12d66fc17db8ec6
shaojunjie <shaojunjie@youzan.com>
2018-04-27 17:25:11 +0800

    add use of break

---
+++ simple_python_break.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @wufeng 2018-04-27
+
+while True:
+    text = input("please input some words : ")
+    if text == 'quit':
+        break
+    print("the length of the words are : " ,len(text))
+print("done")
